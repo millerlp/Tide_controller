4d78df84e2a3bfe3ebd694cfc51695b4345dc85a
Luke Miller <millerlp@gmail.com>
2012-05-03 12:52:02 -0700

    Initial commit. Getting repository set up."

---
+++ Tide_controller.ino
@@ -0,0 +1,6 @@
+/* Tide_controller_v1.0 Luke Miller May 2012
+  This program is designed to calculate the current tide height
+  and control a motor that is changing the water level in a tank.
+
+
+*/
